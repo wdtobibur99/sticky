990d2aec8b84c1dca68ad1e189955b2a3709c5f2
Tobibur Rahman <rtobibur@yahoo.com>
2016-03-31 00:05:46 +0600

    Create style.css

---
+++ css/style.css
@@ -0,0 +1,18 @@
+(function($){
+	$(document).ready(function(){
+		function sticky_relocate() {
+			var window_top = $(window).scrollTop();
+			var div_top = $('#menu-one').offset().top;
+			if (window_top > div_top) {
+				$('#sticky').addClass('stick');
+			} else {
+				$('#sticky').removeClass('stick');
+			}
+		}
+
+		$(function () {
+			$(window).scroll(sticky_relocate);
+			sticky_relocate();
+		});
+	})   
+})(jQuery)
